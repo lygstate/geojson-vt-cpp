ab7c41969b71d5d9712392b3b5544a96a6af99f9
Yonggang Luo <luoyonggang@gmail.com>
2017-03-10 14:35:50 +0800

    Getting geometry works with Visual Studio.
    # Conflicts:
    #	include/mapbox/geometry/geometry.hpp

--- include/mapbox/geometry/geometry.hpp
+++ include/mapbox/geometry/geometry.hpp
@@ -33,7 +33,18 @@ template <typename T, template <typename...> class Cont = std::vector>
 struct geometry : geometry_base<T, Cont>
 {
     using coordinate_type = T;
-    using geometry_base<T>::geometry_base;
+    using geometry_base_type = geometry_base<T>;
+
+    template <typename U>
+    geometry(const U &_v) :geometry_base_type(_v) {}
+    template <typename U>
+    geometry(U&& _v) :geometry_base_type(_v) {}
+
+    /*
+     * The default constructor would create a point geometry with default-constructed coordinates;
+     * i.e. (0, 0). Since this is not particularly useful, and could hide bugs, it is disabled.
+     */
+    geometry() = delete;
 };
 
 template <typename T, template <typename...> class Cont>
